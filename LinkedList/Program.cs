8dfe68fe754c38f59645ac74ee9709af0107eda0
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-28 18:36:36 +0530

    Welcome To the LinkedList Programs

---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello welcome to Linked List Problem");
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddNode(56);
+            customLinkedList.AddNode(30);
+            customLinkedList.AddNode(70);
+            
+            Console.ReadLine();
+        }
+    }
+}
